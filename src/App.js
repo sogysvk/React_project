19696c841b64be28611711c65db07765b3c27c56
Marek Tomas <sogysvk@gmail.com>
2018-11-27 12:49:00 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://google.sk"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            najdi nieco cez googel
+          </a>
+        </header>
+          <form>
+              <font-face-name>NAME</font-face-name>
+          </form>
+      </div>
+    );
+  }
+}
+
+
+
+export default App;
